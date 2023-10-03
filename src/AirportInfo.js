40451a923f8b3d308facdef8080c2d51e0cdd96d
thebestlearner-ric <watto1990@gmail.com>
2023-10-03 23:37:05 +0800

    change frontend to display more information

--- src/AirportInfo.js
+++ src/AirportInfo.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { API_BASE_URL } from './constants';
 
 function AirportInfo() {
   const [icaoCode, setIcaoCode] = useState('');
@@ -8,8 +9,20 @@ function AirportInfo() {
     setIcaoCode(e.target.value);
   };
 
-  const getAirportInfo = async () => {
-    const response = await fetch(`http://backend-service:5000/all_airports`);
+  const getAllAirportInfo = async () => {
+    const response = await fetch(`${API_BASE_URL}/all_airports`);
+    const data = await response.json();
+    setAirportInfo(data);
+    setJsonString(JSON.stringify(data, null, 2));
+  };
+  const getSidsAirportInfo = async () => {
+    const response = await fetch(`${API_BASE_URL}/sids/airport/${icaoCode}`);
+    const data = await response.json();
+    setAirportInfo(data);
+    setJsonString(JSON.stringify(data, null, 2));
+  };
+  const getStarsAirportInfo = async () => {
+    const response = await fetch(`${API_BASE_URL}/stars/airport/${icaoCode}`);
     const data = await response.json();
     setAirportInfo(data);
     setJsonString(JSON.stringify(data, null, 2));
@@ -20,23 +33,31 @@ function AirportInfo() {
       <h2>Get Airport Information</h2>
       <div>
         <label>
-          ICAO Code:
+          Get Specific Airports Info:
           <input
             type="text"
             value={icaoCode}
             onChange={handleInputChange}
           />
         </label>
-        <button onClick={getAirportInfo}>Get Info</button>
+        <button onClick={getSidsAirportInfo}>Get Sids Info</button>
+        <button onClick={getStarsAirportInfo}>Get Stars Info</button>
       </div>
       {airportInfo && (
         <div>
           <h3>Airport Information</h3>
           <p>Name: {airportInfo.name}</p>
-          <p>City: {airportInfo.city}</p>
-          <p>Country: {airportInfo.country}</p>
+          <p>City: {airportInfo.uid}</p>
+          <p>Country: {airportInfo.icao}</p>
         </div>
       )}
+      <div>
+        <h2>Get All Airport Info</h2>
+        <label>
+          Get All Airports Info:
+        </label>
+        <button onClick={getAllAirportInfo}>Get All Info</button>
+      </div>
       {jsonString && (
         <div>
           <h3>JSON String</h3>
